b883b942de434f6b6a52d4378effd6dc490f7211
ledanhthuong <84622574+ledanhthuong@users.noreply.github.com>
2024-11-16 07:37:23 +0700

    good

--- ChatGPT_12_CONGIAP/Program.cs
+++ ChatGPT_12_CONGIAP/Program.cs
@@ -1,24 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
+// Use CORS policy
+app.UseCors("AllowAll");
 
 app.MapControllerRoute(
     name: "default",
